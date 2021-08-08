6e8050f9c2b2e3ff9b07bd2cf234f91e3fc3df9e
DevPed <pedromfr91@gmail.com>
2021-08-08 07:06:53 +0200

    Map

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,3 +1,4 @@
+
 import { alpha, makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles((theme) => ({
@@ -18,15 +19,15 @@ export default makeStyles((theme) => ({
         [theme.breakpoints.up('sm')]: { marginLeft: theme.spacing(3), width: 'auto' },
     },
     searchIcon: {
-        padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none'
+        padding: theme.spacing(0, 2), height: '100%', position: 'absolute', pointerEvents: 'none', display: 'flex', alignItems: 'center', justifyContent: 'center',
     },
     inputRoot: {
         color: 'inherit',
     },
     inputInput: {
-        padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+        padding: theme.spacing(1, 1, 1, 0), paddingLeft: `calc(1em + ${theme.spacing(4)}px)`, transition: theme.transitions.create('width'), width: '100%', [theme.breakpoints.up('md')]: { width: '20ch' },
     },
     toolbar: {
         display: 'flex', justifyContent: 'space-between',
     },
-}))
+}));
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import GoogleMapReact from 'google-map-react';
+import { Paper,Typography,useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab';
+
+import useStyles from './styles';
 
 const Map=()=>{
+    const classes=useStyles();
+    const isMobile=useMediaQuery('(min-width:600px');
+
+    const coordinates={lat:36.72,lng:-4.42};
+
     return (
-        <h1>Map</h1>
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+            bootstrapURLKeys={{key:'AIzaSyDc2aggf9e7uYjy14eaaEStrG7Hohfd898'}}
+            defaultCenter={coordinates}
+            center={coordinates}
+            defaultZoom={15}
+            margin={[50,50,50,50]}
+            options={''}
+            onChange={''}
+            onChildClick={''}>
+                
+            </GoogleMapReact>
+
+        </div>
     );
 }
 
