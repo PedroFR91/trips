e0e96bff84feb35f50f4d1ae3db3be5dd53289a9
DevPed <pedromfr91@gmail.com>
2021-08-08 08:18:27 +0200

    List

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,8 +1,52 @@
-import React from 'react';
+import React,{useState} from 'react';
+import {CircularProgress,Grid,Typography,InputLabel,MenuItem,FormControl,Select} from '@material-ui/core';
+import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles';
 
 const List=()=>{
+    const classes=useStyles();
+    const [type,setType]=useState('restaurants');
+    const [rating,setRating]=useState('');
+
+    const places=[
+        {name:'Sitio de interés'},
+        {name:'Buenas copas'},
+        {name:'Buenas tapas'},
+        {name:'Sitio de interés'},
+        {name:'Buenas copas'},
+        {name:'Buenas tapas'},
+        {name:'Sitio de interés'},
+        {name:'Buenas copas'},
+        {name:'Buenas tapas'},
+    ]
     return (
-        <h1>List</h1>
+       <div className={classes.container}>
+           <Typography variant="h4">Restaurantes, Hoteles y lugares de interés</Typography>
+           <FormControl className={classes.formControl}>
+               <InputLabel>Type</InputLabel>
+               <Select value={type} onChange={(e)=>setType(e.target.value)}>
+                   <MenuItem value="restaurants">Restaurantes</MenuItem>
+                   <MenuItem value="hotel">Hoteles</MenuItem>
+                   <MenuItem value="attractions">Lugares de interés</MenuItem>
+               </Select>
+           </FormControl>
+           <FormControl className={classes.formControl}>
+               <InputLabel>Valoración</InputLabel>
+               <Select value={rating} onChange={(e)=>setRating(e.target.value)}>
+                   <MenuItem value={0}>Todos</MenuItem>
+                   <MenuItem value={3}>Más de 3</MenuItem>
+                   <MenuItem value={4}>Más de 4</MenuItem>
+                   <MenuItem value={4.5}>Más de 4.5</MenuItem>
+               </Select>
+           </FormControl>
+            <Grid container spacing={3} className={classes.list}>
+                {places?.map((place,i)=>(
+                    <Grid item key={i} xs={12}>
+                    <PlaceDetails place={place}/>
+                    </Grid>
+                ))}
+                </Grid>
+       </div>
     );
 }
 
